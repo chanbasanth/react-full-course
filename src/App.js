81659d9b5d088587cedc1dabb73d5e2cc5fa6972
chandu <channubasanth@gmail.com>
2025-02-28 16:02:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React , {useState} from "react"; 
+import "./App.css";
+
+function App() {
+  useState()
+  useState()
+  return (
+    <div className="cvv">
+      <h1>Hello Hooks</h1>
+    </div>
+  );
+}
+
+export default App;
